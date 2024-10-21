063c612f8d092b7ea36f2fc3dd1d27329267dcda
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-21 22:18:32 +0600

    Add user input for customizable half pyramid height

--- src/com/JavaFunctions/HalfPyramid.java
+++ src/com/JavaFunctions/HalfPyramid.java
@@ -1,8 +1,17 @@
 package com.JavaFunctions;
 
+import java.util.Scanner;
+
 public class HalfPyramid {
     public static void main(String[] args) {
-        for(int i = 1; i <= 5; i++){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows for the half pyramid: ");
+        int rows = input.nextInt();
+
+
+        for(int i = 1; i <= rows; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print("* ");
             }
