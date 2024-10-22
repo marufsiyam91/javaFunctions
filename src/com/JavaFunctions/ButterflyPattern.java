5603f03cf1bdbcb60fa6a338f57a62bad5dfb780
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-22 22:48:16 +0600

    print butterfly pattern with multiple loops (took help to solve this problem)

---
+++ src/com/JavaFunctions/ButterflyPattern.java
@@ -0,0 +1,38 @@
+package com.JavaFunctions;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+
+        for(int i = 1; i <= rows; i++){
+
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            int spaces = 2 * (rows - i);
+            for (int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i = rows; i >= 1; i--){
+
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            int spaces = 2 * (rows - i);
+            for (int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
